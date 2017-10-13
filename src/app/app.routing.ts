877690b53ec88c8bd4ab643225eef538854ab6eb
kaiser <jannis.kaiser@adesso.de>
2017-10-13 09:39:09 +0200

    add route for books and mouse

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,6 +1,18 @@
 import {ModuleWithProviders} from '@angular/core';
-import {Routes, RouterModule, Route} from '@angular/router';
+import {Routes, RouterModule} from '@angular/router';
+import {BookListComponent} from './book-list/book-list.component';
+import {MouseCursorComponent} from './mouse-cursor/mouse-cursor.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'books',
+  component: BookListComponent
+}, {
+ path: 'mouse',
+ component: MouseCursorComponent
+}, {
+  path: '',
+  redirectTo: 'books',
+  pathMatch: 'full'
+}];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(routes);
