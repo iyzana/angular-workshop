a5553229d6c2817fb49be07c9654a623bbb53a65
kaiser <jannis.kaiser@adesso.de>
2017-10-13 16:33:03 +0200

    cleanup import

--- src/app/book/book-new/book-new.component.ts
+++ src/app/book/book-new/book-new.component.ts
@@ -1,6 +1,5 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {hasOwnProperty} from 'tslint/lib/utils';
 import {BookDataService} from '../shared/book-data.service';
 
 @Component({
